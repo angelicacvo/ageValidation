360536660d17d73b5103e731a3990735cd97e43d
angelicacvo <108093638+angelicacvo@users.noreply.github.com>
2025-06-19 00:03:57 -0500

    validations added

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let name = prompt("Please enter your name: ");
 let age = prompt("Please enter your age: ");
-
 age = parseInt(age);
 
-if (age < 18){
-    alert("Hola ${name} ")
+if (isNaN(age)) {
+  console.error("value is not valid. Keep trying.");
+  alert(`Hi ${name}, please enter a valid number`);
+} else if (age < 18) {
+  alert(
+    `Hi ${name}, you are under 18 years old. Keep learning to become an excellent programmer.`
+  );
+} else {
+  alert(
+    `Hi ${name}, you're over 18 years old. You're ready to become a programmer. Good luck!`
+  );
 }
-    
-
